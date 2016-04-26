586a63163b284afc21344538f5852791f71b3e75
Patrick Weisensee <pweisensee@gmail.com>
2016-04-26 01:14:06 -0700

    improved report and readings views

--- app/models/report.rb
+++ app/models/report.rb
@@ -6,9 +6,25 @@ class Report < ActiveRecord::Base
 
   def concat_to_json
     @json = ""
-    readings.within_dates(start_date, end_date).each_with_index do |val, index|
+    readings_subset.each_with_index do |val, index|
       @json = @json + "[#{index}, #{val.glucose_level}, #{val.created_at.to_i*1000}],"
     end
     @json.to_s.chomp(',')
+  end
+
+  def average_glucose
+    readings_subset.average(:glucose_level)
+  end
+
+  def max_glucose
+    readings_subset.maximum(:glucose_level)
+  end
+
+  def min_glucose
+    readings_subset.minimum(:glucose_level)
+  end
+
+  def readings_subset
+    readings.within_dates(start_date, end_date)
   end
 end
